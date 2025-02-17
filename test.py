3f85d7435023455e54f81d9d6b018a50f9475d14
Angela Liu <yingchenliu@wireless-10-145-0-108.public.utexas.edu>
2025-02-17 16:58:05 -0600

    test

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-'''
-    for i in range(NUM_LETTERS):
-        char = guessed_word[i]
-        if char in secret_word and count[char] > 0:
-            # right spot
-            if char == secret_word[i]:
-                feedback[i] = CORRECT_COLOR
-                count[char] -= 1
-            # wrong spot
-            else:
-                feedback[i] = WRONG_SPOT_COLOR
-                count[char] -= 1
-        else:
-            # wrong guess
-            feedback[i] = NOT_IN_WORD_COLOR
-'''
+print(len([]))
